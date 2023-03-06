7599d33f326c5cc3c2cfc9eebdb29a2c1f0d49dc
Andrew Pham <andrew.pham@shopify.com>
2023-03-06 04:27:31 -0500

    Remove node package

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./flaskbook_app/templates/**/*.html', './flaskr/static/**/*.js'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
